8209348efa3f0d1aafe985473eff373f93ee4cf0
ashasugave <ashasugave27@gmail.com>
2023-10-01 13:13:07 +0530

    completed first ass

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName;// variable declaration
+
+firstName = "Asha"; //initialization
+console.log(firstName);
+firstName = "Amar"// Update
+console.log(firstName);
+
+var lastName = "Sugave"// variable declaration and initialization
+console.log(lastName);
+
+var hight;// declaration
+console.log(hight);
+hight = 6;// initialization
+console.log(hight);
+hight = 6.2;//Update
+console.log(hight);
---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName;
+console.log(myFullName);
+var myFullName = "Asha Sugave";
+console.log(myFullName);
+myFullName = "Asha Shivhar Sugave";
+console.log(myFullName);
+
+var myLove = "I lone only JS";
+console.log(myLove);
+
+var firstName = "Asha";
+console.log("First Name:", firstName);
+
+var lastName = "Sugave";
+console.log("Last Name:", lastName);
+
+var age = "24";
+console.log("Age:", age);
+
+var address = "Pune";
+console.log("Address:", address);
+
+var pinCode = "411057";
+console.log("Pincode:", pinCode);
+
+var gender ="Female";
+console.log("Gender:", gender);
+
+var isMarried = "Yes" ;
+console.log("Is Married:", isMarried);
