b4b6f48bc8ee3e1d9f458a9e1dba6c3ce0adb8f4
ashasugave <ashasugave27@gmail.com>
2023-10-02 20:37:49 +0530

    Assignment 2

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,22 @@
+var age = 24;
+console.log("Age:" , age, "Type:" , typeof age);
+var rollNumber = 101;
+console.log("Roll Number:" , rollNumber, "Type:" , typeof rollNumber);
+var mobNo = 1234567890;
+console.log("Mob No:" , mobNo, "Type:", typeof mobNo);
+
+var bankName = "Axis";
+console.log("Bank Name:", bankName, "Type:", typeof bankName);
+var company = "CodeMind";
+console.log("Company:", company, "Type:", typeof company);
+var branch = "Computer";
+console.log("Branch:", branch, "Type:", typeof branch);
+
+var isMarried = true;
+console.log("Is Married:", isMarried, "Type:", typeof isMarried);
+var isHandicap = false;
+console.log("Is Handicap:", isHandicap, "Type:", typeof isHandicap);
+var isGraduated = true;
+console.log("Is Graduatd", isGraduated, "Type:", typeof isGraduated);
+
+console.log("-----------NULL means no value, undefined means when we declare only variable thats called undefined");
