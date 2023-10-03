01adb795207262d6c8b4f2767ecee0b190f7ee3f
ashasugave <ashasugave27@gmail.com>
2023-10-03 21:58:01 +0530

    Assignment 3

---
+++ 03_funAssig02.js
@@ -0,0 +1,8 @@
+
+function bankDetails(bankName,accountNum,location,pinCode){
+
+    console.log(bankName,accountNum,location,pinCode);
+}
+bankDetails("CITI Bank",1234567890,"Pune",413520);
+bankDetails("Axis Bank",3456783456,"Mumbai",986798);
+bankDetails("HDFC Bank",4567892345,"Pune",234567);
